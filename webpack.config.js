cc6451cdd630736a938d4a26207f6068d7b845bb
seeliang <me@sihuiliang.com>
2016-01-04 21:51:38 +1100

    initial for lib

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,19 @@
 /*eslint-disable */
 var webpack = require('webpack'),
   CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
+
 module.exports = {
   entry: {
     app: './assets/js/src/app.js',
   },
+  externals: {
+      'React': 'react',
+      'render': 'react-dom'
+  },
   output: {
     path: ('./build/js'),
     publicPath:'build/js',
-    filename: '[name]-bundle.js'
+    filename: 'react-range-selector.js'
   },
   resolve: {
     modulesDirectories: [
@@ -20,7 +25,8 @@ module.exports = {
    loaders: [{
      test: /\.jsx?$/, // A regexp to test the require path. accepts either js or jsx
      loader: 'babel' // The module to load. "babel" is short for "babel-loader"
-   }]
+   }],
+   noParse: [ 'react', 'react-dom']
  },
   plugins: [
     //new webpack.optimize.UglifyJsPlugin({ output: {comments: false}})
