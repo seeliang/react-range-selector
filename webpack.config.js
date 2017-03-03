d5aa4705d4d4fc25b35eddcb545b9dc2dd2c3d19
Sihui liang <me@sihuiliang.com>
2017-03-04 10:58:20 +1100

    upgrade to web pack 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,38 @@
-
-/*eslint-disable */
-var webpack = require('webpack'),
+const webpack = require('webpack'),
+  path =  require('path'),
   CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
 
 module.exports = {
   entry: {
     app: './assets/js/src/app.js',
   },
+
   externals: {
       'React': 'react',
       'render': 'react-dom'
   },
+
+  module: {
+    rules: [{
+      test: /\.jsx?$/, // A regexp to test the require path. accepts either js or jsx
+      loader: 'babel-loader'
+      }],
+  },
+
   output: {
-    path: ('./build/js'),
+    path: (path.join(__dirname,'/build/js')),
     publicPath:'build/js',
     filename: 'react-range-selector.js'
   },
-  resolve: {
-    modulesDirectories: [
-      'node_modules'
-    ]
-  },
-  module: {
-   loaders: [{
-     test: /\.jsx?$/, // A regexp to test the require path. accepts either js or jsx
-     loader: 'babel' // The module to load. "babel" is short for "babel-loader"
-   }],
-   noParse: [ 'react', 'react-dom']
- },
+
   plugins: [
     new webpack.optimize.UglifyJsPlugin({ output: {comments: false}})
-  ]
+  ],
+
+  resolve: {
+    modules: [
+      'node_modules'
+    ]
+  }
+
 };
