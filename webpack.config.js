4fcbe6539d3b5aaa2c740ccffa453c048eda80f4
Sihui liang <me@sihuiliang.com>
2017-03-05 17:29:18 +1100

    remove extra web pack settings

--- webpack.config.js
+++ webpack.config.js
@@ -26,11 +26,6 @@ module.exports = {
   },
 
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
     new webpack.optimize.UglifyJsPlugin({
       comments: false
     })
