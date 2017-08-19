42be95275a23e31f22561387bd89b5c950dd5ef3
Sihui liang <me@sihuiliang.com>
2017-08-19 12:06:34 +1000

    update html build and add dist clean

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,20 @@
-const gulp = require('gulp');
+const clean = require('gulp-clean'),
+  gulp = require('gulp'),
+  paths = {
+    dist: './dist'
+  },
+  replace = require('gulp-replace');
+
+
 require('sl-gt-synclint')(gulp);
 
-gulp.task('html:copy', () => {
-  gulp.src('./index.html')
-    .pipe(gulp.dest('./dist/'));
+gulp.task('html', () => {
+  return gulp.src('./index.html')
+    .pipe(replace('http://localhost:8080/dist/',''))
+    .pipe(gulp.dest(paths.dist));
+})
+
+gulp.task('clean',() => {
+  return gulp.src(paths.dist)
+  .pipe(clean());
 })
