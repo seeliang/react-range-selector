5cb3e059928badd16e85d74d335d9817a78e46ac
Sihui liang <me@sihuiliang.com>
2017-08-25 08:38:58 +1000

    add loading page

---
+++ src/atomic/pages/loading.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Page extends React.Component {
+  displayName: 'Page';
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <h1> loading ... </h1>
+    );
+  }
+}
+
+export default Page;
