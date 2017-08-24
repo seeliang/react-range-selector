93f31bcfd4a3e161041e2e92b0e805909cf9a0f0
Sihui liang <me@sihuiliang.com>
2017-08-24 16:15:41 +1000

    add thunk and chrome redux dev tool

--- src/js/store.js
+++ src/js/store.js
@@ -1,16 +1,24 @@
 import {createStore, compose} from 'redux';
+import middleware from './middleware';
 import reducers from './reducers';
 
 
-const initialState = {
-  feed: {
-    apiFetched: '',
-    apiTarget: '',
-    fetched: false
-  }
-};
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    }) : compose,
+  enhancers = composeEnhancers(middleware),
+  initialState = {
+    feed: {
+      apiFetched: '',
+      apiTarget: '',
+      fetched: false
+    }
+  };
 
 export default createStore(
   reducers,
-  initialState
+  initialState,
+  enhancers
 );
