bed362c943fa284f39a5e945658c5be64117f543
Sihui liang <me@sihuiliang.com>
2017-08-25 22:34:44 +1000

    add reload for server side issue

--- src/atomic/pages/page.js
+++ src/atomic/pages/page.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import UserLink from '../atoms/link';
+import Button from '../atoms/button';
 import Loading from './loading';
 
 class Page extends React.Component {
   displayName: 'Page';
   constructor(props) {
     super(props);
+    this.reload = () => {
+      this.load();
+    };
   }
 
   lists() {
@@ -22,7 +26,25 @@ class Page extends React.Component {
       });
   }
 
+  load() {
+    let params = this.props.feed.apiTarget;
+    if (params.localeCompare(this.props.feed.apiFetched) !== 0) {
+      this.props.feedFetch(params);
+    }
+  }
+
   screen() {
+    if(typeof this.props.feed.fetchError === 'boolean' && this.props.feed.fetchError === true) {
+      return (
+        <div>
+          <h1> oh~ no, we have some issue from server side </h1>
+          <Button action={this.reload}
+            name="Try it again"
+          />
+        </div>
+      );
+    }
+
     if(this.props.feed.fetched === false || this.props.feed.feed.results.length < 1) {
       return <Loading />;
     }
