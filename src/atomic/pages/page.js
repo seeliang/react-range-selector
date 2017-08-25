1836afa52b1a4d7ffb836f0d516efcf2f59298c8
Sihui liang <me@sihuiliang.com>
2017-08-25 21:12:05 +1000

    page: add link atom and loading

--- src/atomic/pages/page.js
+++ src/atomic/pages/page.js
@@ -1,18 +1,43 @@
-import {Link} from 'react-router-dom';
 import React from 'react';
+
+import UserLink from '../atoms/link';
+import Loading from './loading';
 
 class Page extends React.Component {
   displayName: 'Page';
   constructor(props) {
     super(props);
   }
+
+  lists() {
+    return this.props.feed.feed.results.map(
+      (user,i) => {
+        return (
+          <UserLink
+            key={i}
+            name={user.name}
+            to={'/people/' + i}
+          />
+        );
+      });
+  }
+
+  screen() {
+    if(this.props.feed.fetched === false || this.props.feed.feed.results.length < 1) {
+      return <Loading />;
+    }
+
+    return (
+      <div>
+        {this.lists()}
+      </div>
+    );
+  }
+
   render() {
     return (
       <div>
-        <h1> Hello, i am the other page</h1>
-        <Link to={'/'}>
-          Go to home
-        </Link>
+        {this.screen()}
       </div>
     );
   }
