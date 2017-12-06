4d3c5d15c7bddc1b18c432d89fe5c24f7503a312
seeliang <me@sihuiliang.com>
2017-12-06 17:29:34 +1100

    update select function test

---
+++ src/__tests__/click__test.js
@@ -0,0 +1,51 @@
+/* eslint-disable */
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import RangeSelector from '../index';
+import {shallow, mount} from 'enzyme';
+import React from 'react';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+it('empty range click', () => {
+  const tree = shallow(<RangeSelector componentName={'range-selector'}
+      key={3}
+      name={'car'}
+      range={[3,5]}
+  />);
+  tree.instance().updateSelected(4);
+  expect(tree.instance().state.selected).toEqual([4]);
+});
+
+it('two points click', () => {
+  const tree = shallow(<RangeSelector componentName={'range-selector'}
+      initalSelected={[3,4]}
+      key={3}
+      name={'car'}
+      range={[3,5]}
+  />);
+  tree.instance().updateSelected(5);
+  expect(tree.instance().state.selected).toEqual([5]);
+});
+
+it('one point to two points range click', () => {
+  const tree = shallow(<RangeSelector componentName={'range-selector'}
+      initalSelected={[4]}
+      key={3}
+      name={'car'}
+      range={[3,5]}
+  />);
+  tree.instance().updateSelected(5);
+  expect(tree.instance().state.selected).toEqual([4,5]);
+});
+
+it('one point to cancel click', () => {
+  const tree = shallow(<RangeSelector componentName={'range-selector'}
+      initalSelected={[4]}
+      key={3}
+      name={'car'}
+      range={[3,5]}
+  />);
+  tree.instance().updateSelected(4);
+  expect(tree.instance().state.selected).toEqual([]);
+});
