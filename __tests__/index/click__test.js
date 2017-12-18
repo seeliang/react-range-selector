4bafb420aebce12f01da94b53de8749cc4083a75
seeliang <me@sihuiliang.com>
2017-12-19 08:29:13 +1100

    move main test out of src

--- __tests__/index/click__test.js
+++ __tests__/index/click__test.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import RangeSelector from '../index';
+import RangeSelector from '../../src/index';
 import {shallow} from 'enzyme';
 import React from 'react';
 
