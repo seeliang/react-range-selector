fdb7c643b25ff6fd223f90950db7b46b8f913085
seeliang <me@sihuiliang.com>
2020-01-02 18:10:37 +1100

    update demo for react usage

--- demo/import-set/form.js
+++ demo/import-set/form.js
@@ -3,35 +3,58 @@ import {render} from 'react-dom';
 import RangeSelector from '../../src/index';
 
 
+const alertValues = values => {
+  alert(values);
+}
+
 class Form extends React.Component {
   displayName: 'TestForm';
   constructor(props) {
     super(props);
+    this.range = {
+      cores:[3,8],
+      frequency: [2.5,3.4,4.2,6.3]
+    }
     this.state = {
-      sets: {
-        doors: [3,5]
+      index: {
+        cores: [3,5],
+        frequency: [1,2]
       }
     };
-
-    this.rangeUpdate = this.rangeUpdate.bind(this);
   }
 
   rangeUpdate(range,section) {
     this.setState((prev) => {
-      let newSet = prev.sets;
+      let newSet = prev.index;
       newSet[section] = range;
-      return {sets: newSet};
+      return {index: newSet};
     });
+  }
+  submit(e) {
+    e. preventDefault();
+    alert(JSON.stringify(this.state))
   }
 
   render() {
     return (
+      <form >
+      <fieldset>
       <RangeSelector componentName={'range-selector'}
-        initalSelected={this.state.sets.doors}
-        name={'doors'}
-        range={[3,8]}
-        rangeUpdate={this.rangeUpdate}
+        initalSelected={this.state.index.cores}
+        name={'cores'}
+        range={this.range.cores}
+        rangeUpdate={(r,o) => this.rangeUpdate(r,o)}
       />
+      <RangeSelector componentName={'range-selector'}
+        initalSelected={this.state.index.frequency}
+        name={'frequency'}
+        customiseRange={this.range.frequency}
+        rangeUpdate={(r,o) => this.rangeUpdate(r,o)}
+      />
+      <input type="submit" value="submit" onClick={e => this.submit(e)}/>
+      </fieldset>
+      
+      </form>
     );
   }
 }
