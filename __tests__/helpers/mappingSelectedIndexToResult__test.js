00aeeaa4ac4cbe95ea783070e9a3d5e7c3748ff4
seeliang <me@sihuiliang.com>
2020-01-04 18:30:27 +1100

    chore: improve test coverage

--- __tests__/helpers/mappingSelectedIndexToResult__test.js
+++ __tests__/helpers/mappingSelectedIndexToResult__test.js
@@ -1,5 +1,17 @@
 import {mappingSelectIndexToResult} from '../../src/helpers';
 
-it('should covert range to list with correct range', () => {
+it('should covert range of two points to list with correct values', () => {
   expect(mappingSelectIndexToResult({list:[3,4,5,6], selectIndex: [1,3]})).toMatchObject([4,5,6]);
+});
+
+it('should covert range of one points to list with correct range', () => {
+  expect(mappingSelectIndexToResult({list:[3,4,5,6], selectIndex: [1]})).toMatchObject([4]);
+});
+
+it('should throw if list is not an array', () => {
+  expect(() => {mappingSelectIndexToResult({list: {}, selectIndex: [1]});}).toThrow();
+});
+
+it('should throw if list is empty array', () => {
+  expect(() => {mappingSelectIndexToResult({list: [], selectIndex: [1]});}).toThrow();
 });
