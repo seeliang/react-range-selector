fdf90e6b7f274a723521cd3a53896eab22e3c491
seeliang <me@sihuiliang.com>
2020-01-04 16:58:52 +1100

    update demo for initial handle selectors

--- demo/import-set/form.js
+++ demo/import-set/form.js
@@ -1,37 +1,62 @@
 import React from 'react';
 import {render} from 'react-dom';
-import RangeSelector, {rangeToList, mappingSelectIndexToResult} from '../../src/index';
+import RangeSelector, {initialFormState} from '../../src/index';
 
-
-const type = {
-  cores: 'range',
-  frequency: 'list'
-};
-
-const range = {
-  cores:[3,8],
-  frequency: [2.5,3.4,4.2,6.3]
-};
+const selectors = [
+  {
+    name: 'Cores',
+    range: [3,8]
+  },
+  {
+    name: 'Frequency',
+    customiseRange: [2.5,3.4,4.2,6.3],
+    initialSelected: [0,2]
+  },
+  {
+    name: 'PCIE slots',
+    range: [1,4],
+    initialSelected: [0,1]
+  },
+  {
+    name: 'Ram size',
+    customiseRange: [2,4,6,8],
+    initialSelected: [2,3]
+  }
+];
 
 class Form extends React.Component {
   displayName: 'TestForm';
   constructor(props) {
     super(props);
-    this.initialSelected = {
-      cores: [3,5],
-      frequency: [2]
-    };
-    this.lists = {
-      cores: rangeToList(range.cores),
-      frequency: range.frequency
-    };
     this.state = {
-      indexRange: this.initialSelected,
-      values: {
-        cores: mappingSelectIndexToResult({list: this.lists.cores, selectIndex: this.initialSelected.cores}),
-        frequency: mappingSelectIndexToResult({list: this.lists.frequency, selectIndex: this.initialSelected.frequency})
-      }
+      ...initialFormState(selectors)
     };
+  }
+
+  listOfSelectors () {
+    return selectors.map(
+      (selector) => {
+        const { name,initialSelected,customiseRange, range} = selector;
+        let rangeData = {};
+        if (customiseRange) {
+          rangeData = {customiseRange};
+        }
+        if (range) {
+          rangeData = {range};
+        }
+        const props = {
+          key: name,
+          componentName: 'range-selector',
+          initialSelected,
+          name,
+          rangeUpdate: data => this.rangeUpdate(data),
+          ...rangeData
+        };
+        return (
+          <RangeSelector {...props}/>
+        );
+      }
+    );
   }
 
   rangeUpdate({selectedIndex,section,values}) {
@@ -55,18 +80,7 @@ class Form extends React.Component {
     return (
       <form >
         <fieldset>
-          <RangeSelector componentName={'range-selector'}
-            initialSelected={this.state.indexRange.cores}
-            name={'cores'}
-            range={range.cores}
-            rangeUpdate={(data) => this.rangeUpdate(data)}
-          />
-          <RangeSelector componentName={'range-selector'}
-            customiseRange={range.frequency}
-            initialSelected={this.state.indexRange.frequency}
-            name={'frequency'}
-            rangeUpdate={(data) => this.rangeUpdate(data)}
-          />
+          {this.listOfSelectors()}
           <input onClick={e => this.submit(e)}
             type="submit"
             value="submit"
