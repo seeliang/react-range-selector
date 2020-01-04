cb8d21510e849bfa73be50c4a0a28465a9979f7d
seeliang <me@sihuiliang.com>
2020-01-04 14:48:56 +1100

    get default values

--- demo/import-set/form.js
+++ demo/import-set/form.js
@@ -1,96 +1,78 @@
 import React from 'react';
 import {render} from 'react-dom';
-import RangeSelector from '../../src/index';
+import RangeSelector, {rangeToList, mappingSelectIndexToResult} from '../../src/index';
 
-const rangeWithSelectIndexToResult = (range, selectIndex) => {
-  const start = range[0];
-  let result = [];
-  if (!selectIndex[1]) {
-    result = [start + selectIndex[0]]
-  } else {
-    for (let i = start + selectIndex[0]; i <= start + selectIndex[1] ; i++) {
-      result.push(i);
-    }
-  }
-  return result;
-};
-
-const listWithSelectIndexToResult = (list, selectIndex) => {
-  let result = [];
-  if (!selectIndex[1]) {
-    result = [list[selectIndex[0]]];
-  } else {
-    for (let i = selectIndex[0]; i <= selectIndex[1] ; i++) {
-      result.push(list[i]);
-    }
-  }
-  return result;
-};
 
 const type = {
   cores: 'range',
   frequency: 'list'
-}
+};
 
 const range = {
   cores:[3,8],
   frequency: [2.5,3.4,4.2,6.3]
-}
+};
 
 class Form extends React.Component {
   displayName: 'TestForm';
   constructor(props) {
     super(props);
+    this.initialSelected = {
+      cores: [3,5],
+      frequency: [2]
+    };
+    this.lists = {
+      cores: rangeToList(range.cores),
+      frequency: range.frequency
+    };
     this.state = {
-      indexRange: {
-        cores: [3,5],
-        frequency: [2]
+      indexRange: this.initialSelected,
+      values: {
+        cores: mappingSelectIndexToResult({list: this.lists.cores, selectIndex: this.initialSelected.cores}),
+        frequency: mappingSelectIndexToResult({list: this.lists.frequency, selectIndex: this.initialSelected.frequency})
       }
     };
   }
 
-  rangeUpdate(range,section) {
+  rangeUpdate({selectedIndex,section,values}) {
     this.setState((prev) => {
-      let newSet = prev.indexRange;
-      newSet[section] = range;
-      return {indexRange: newSet};
+      let newSet = prev.indexRange,
+        newValues = prev.values;
+      newSet[section] = selectedIndex;
+      newValues[section] = values;
+      return {indexRange: newSet, values: newValues};
     });
   }
 
   submit(e) {
     e.preventDefault();
-    const { indexRange } = this.state
+    const { indexRange } = this.state;
     const values = {};
-    for(const i in indexRange) {
-      if (type[i] === 'range') {
-        values[i] = rangeWithSelectIndexToResult(range[i],indexRange[i]);
-      }
-      if (type[i] === 'list') {
-        values[i] = listWithSelectIndexToResult(range[i],indexRange[i]);
-      }
-    }
-    alert(JSON.stringify(values))
+    alert(JSON.stringify(this.state));
   }
 
   render() {
     return (
       <form >
-      <fieldset>
-      <RangeSelector componentName={'range-selector'}
-        initialSelected={this.state.indexRange.cores}
-        name={'cores'}
-        range={range.cores}
-        rangeUpdate={(r,o) => this.rangeUpdate(r,o)}
-      />
-      <RangeSelector componentName={'range-selector'}
-        initialSelected={this.state.indexRange.frequency}
-        name={'frequency'}
-        customiseRange={range.frequency}
-        rangeUpdate={(r,o) => this.rangeUpdate(r,o)}
-      />
-      <input type="submit" value="submit" onClick={e => this.submit(e)}/>
-      </fieldset>
-      
+        <fieldset>
+          <RangeSelector componentName={'range-selector'}
+            initialSelected={this.state.indexRange.cores}
+            name={'cores'}
+            range={range.cores}
+            rangeUpdate={(data) => this.rangeUpdate(data)}
+          />
+          <RangeSelector componentName={'range-selector'}
+            customiseRange={range.frequency}
+            initialSelected={this.state.indexRange.frequency}
+            name={'frequency'}
+            rangeUpdate={(data) => this.rangeUpdate(data)}
+          />
+          <input onClick={e => this.submit(e)}
+            type="submit"
+            value="submit"
+          />
+        </fieldset>
+
       </form>
     );
   }
