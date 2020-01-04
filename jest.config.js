ca594c2a398bb241c6dbb9963b2657a6bf8e28fd
seeliang <me@sihuiliang.com>
2020-01-05 08:50:57 +1100

    chore: add export check

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,9 @@ module.exports = {
   unmockedModulePathPatterns: [
     'node_modules/react/',
     'node_modules/enzyme/'
+  ],
+  collectCoverageFrom: [
+    '!__tests__/exportCheck/**/*.*',
+    'src/**/*.*'
   ]
 };
