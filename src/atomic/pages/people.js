4a22ca16cc09b90ffc66c174bb0ca3d402fc4dbf
seeliang <me@sihuiliang.com>
2020-01-10 06:39:43 +1100

    chore: pure function

--- src/atomic/pages/people.js
+++ src/atomic/pages/people.js
@@ -1,31 +1,13 @@
 import React from 'react';
-import Button from '../atoms/button';
-import Loading from './loading';
 
-class People extends React.Component {
-  displayName: 'People';
-  constructor(props) {
-    super(props);
-  }
-
-  lists() {
-    const {feed, match} = this.props;
-    let people = feed.data.results[match.params.id];
-    return (
-      <div>
-        <h1>{people.name}</h1>
-        <h2>height: {people.height}</h2>
-      </div>
-    );
-  }
-
-  render() {
-    return (
-      <div>
-        {this.lists()}
-      </div>
-    );
-  }
-}
+const People = ({feed, match}) => {
+  let people = feed.data.results[match.params.id];
+  return (
+    <div>
+      <h1>{people.name}</h1>
+      <h2>height: {people.height}</h2>
+    </div>
+  );
+};
 
 export default People;
