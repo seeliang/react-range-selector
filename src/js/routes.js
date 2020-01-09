adfee83b4d24a280669d98a464b1ea35a73b2038
seeliang <me@sihuiliang.com>
2020-01-10 08:18:48 +1100

    chore: router to pure func

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , {useEffect}from 'react';
 
 import { HashRouter as Router, Route, Redirect} from 'react-router-dom';
 
@@ -6,60 +6,54 @@ import Loading from '../atomic/pages/loading';
 import Page from '../atomic/pages/page';
 import People from '../atomic/pages/people';
 
-class Routes extends React.Component {
-  displayName: 'Routes';
-  constructor(props) {
-    super(props);
-  }
 
-  componentDidMount () {
-    const {feedFetch, feed} = this.props;
+
+const Routes = ({feedFetch, feed}) => {
+  //demo error fetch
+  useEffect(() => {
     if(feed.status.fetched === false) {
       feedFetch('people.jso');
     }
-  }
+  },[]);
 
-  render() {
-    const {feedFetch, feed} = this.props;
-    return (
-      <Router>
-        <div>
-          <Route exact
-            path="/"
-            render={() => (
-              feed.status.fetched === false ? <Redirect to="/loading" /> :
-                <Redirect to="/page"/>
-            )}
-          />
-          <Route
-            path="/page"
-            render={() => (
-              <Page
-                feed={feed}
-              />
-            )}
-          />
-          <Route
-            path="/loading"
-            render={() => (
-              <Loading feed={feed}
-                feedFetch={feedFetch}
-              />
-            )}
-          />
-          <Route
-            path="/people/:id"
-            render={(props) => (
-              <People
-                feed={feed}
-                {...props}
-              />
-            )}
-          />
-        </div>
-      </Router>
-    );
-  }
-}
+  return (
+    <Router>
+      <div>
+        <Route
+          path="/"
+          render={() => (
+            <Redirect to="/loading" />
+          )}
+        />
+        <Route
+          path="/page"
+          render={() => (
+            <Page
+              feed={feed}
+            />
+          )}
+        />
+        <Route
+          path="/loading"
+          render={() => (
+            <Loading feed={feed}
+              feedFetch={feedFetch}
+            />
+          )}
+        />
+        <Route
+          path="/people/:id"
+          render={(props) => (
+            <People
+              feed={feed}
+              {...props}
+            />
+          )}
+        />
+      </div>
+    </Router>
+  );
+};
+
 
 export default Routes;
