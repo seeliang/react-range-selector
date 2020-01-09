1e4f4eb3e1dc1afb2978c9c69ebd9e5d30d9c80c
seeliang <me@sihuiliang.com>
2020-01-09 22:43:35 +1100

    pure function

--- src/atomic/pages/loading.js
+++ src/atomic/pages/loading.js
@@ -2,36 +2,23 @@ import React from 'react';
 import Button from '../atoms/button';
 import { Redirect } from 'react-router-dom';
 
-class Loading extends React.Component {
-  displayName: 'Loading';
-  constructor(props) {
-    super(props);
+const Loading = ({feed, feedFetch}) => {
+  if(typeof feed.status.fetchError === 'boolean' && feed.status.fetchError === true) {
+    return (
+      <div>
+        <h1> oh~ no, we have some issue from server side </h1>
+        <Button action={() => feedFetch('people.json')}
+          name="Try it again"
+        />
+      </div>
+    );
+  }
+  if(feed.status.fetched === false || feed.data.results.length < 1) {
+    return  <h1> loading ... </h1>;
   }
 
-  load() {
-    this.props.feedFetch('people.json');
-  }
+  return <Redirect to="/page"/>;
+};
 
-  render() {
-    const {feed} = this.props;
-    if(typeof feed.status.fetchError === 'boolean' && feed.status.fetchError === true) {
-      return (
-        <div>
-          <h1> oh~ no, we have some issue from server side </h1>
-          <Button action={() => this.load()}
-            name="Try it again"
-          />
-        </div>
-      );
-    }
-
-    if(feed.status.fetched === false || feed.data.results.length < 1) {
-      return  <h1> loading ... </h1>;
-    }
-
-    return <Redirect to="/page"/>;
-
-  }
-}
 
 export default Loading;
