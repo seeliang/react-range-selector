433656971fa987945d31c1eae21e748276fb3b75
seeliang <me@sihuiliang.com>
2020-01-10 06:25:03 +1100

    chore: Pure function

--- src/js/app.js
+++ src/js/app.js
@@ -4,17 +4,10 @@ import {render} from 'react-dom';
 import Redux from './redux';
 import Routes from './routes';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <Redux
-        content={Routes}
-      />
-    );
-  }
-}
+const App = () => (
+  <Redux
+    content={Routes}
+  />
+);
 
 render(<App/>, document.getElementById('app'));
